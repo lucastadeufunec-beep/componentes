7da78a69cb1a747dc62f319bbbdb8e487036e9bd
Bernardogit29 <bernardofunec@gmail.com>
2026-09-17 13:57:04 -0300

    Adicionando novos arquivos

---
+++ main.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/functions/server.php';
+require_once __DIR__ . '/functions/index.php';
+require_once __DIR__ . '/functions/router.php';
+require_once __DIR__ . '/functions/middleware.php';
+require_once __DIR__ . '/functions/dispatcher.php';
+require_once __DIR__ . '/functions/usuario.service.php';git add .
